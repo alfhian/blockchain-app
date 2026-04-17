ad17375ecf1f1b1fa2fe0b113d5f0b1476c8d458
Alfhian Suwandi <30432929+alfhian@users.noreply.github.com>
2026-04-18 06:52:33 +0700

    Reduce startup hang risk by simplifying root modal and Sentry init

--- blockchain-frontend/App.js
+++ blockchain-frontend/App.js
@@ -1,30 +1,18 @@
 import React, { useEffect } from 'react';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
-import { useColorScheme, LogBox } from 'react-native';
-import { Provider as PaperProvider, MD3LightTheme, MD3DarkTheme } from 'react-native-paper';
-import { WalletConnectModal } from '@walletconnect/modal-react-native';
+import { LogBox } from 'react-native';
+import { Provider as PaperProvider, MD3DarkTheme } from 'react-native-paper';
 import AppNavigator from './src/navigation/AppNavigator';
 import { decode as atob, encode as btoa } from 'base-64';
-import { providerMetadata } from './src/config/walletConnect';
 
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import * as Sentry from '@sentry/react-native';
 
 Sentry.init({
   dsn: 'https://ebff32081f096910aa926495f34de437@o4509933839712256.ingest.us.sentry.io/4509933855375360',
-
-  // Adds more context data to events (IP address, cookies, user, etc.)
-  // For more information, visit: https://docs.sentry.io/platforms/react-native/data-management/data-collected/
+  enabled: !__DEV__,
   sendDefaultPii: true,
-
-  // Configure Session Replay
-  replaysSessionSampleRate: 0.1,
-  replaysOnErrorSampleRate: 1,
-  integrations: [Sentry.mobileReplayIntegration(), Sentry.feedbackIntegration()],
-
-  // uncomment the line below to enable Spotlight (https://spotlightjs.com)
-  // spotlight: __DEV__,
 });
 
 // Ignore warnings
@@ -73,29 +61,6 @@ export default Sentry.wrap(function App() {
       <PaperProvider theme={AcademicTheme}>
         <SafeAreaProvider>
           <AppNavigator />
-          <WalletConnectModal
-            explorerRecommendedWalletIds={[
-              'c57ca95b47569778a828d19178114f4db188b89b763c899ba0be274e97267d96',
-            ]}
-            explorerExcludedWalletIds={'ALL'}
-            projectId="1cce4f444c2ea269a7c1cd573eaf9f5c"
-            providerMetadata={providerMetadata}
-            sessionParams={{
-              namespaces: {
-                eip155: {
-                  methods: [
-                    'eth_sendTransaction',
-                    'eth_signTransaction',
-                    'eth_sign',
-                    'eth_signTypedData',
-                    'personal_sign',
-                  ],
-                  chains: ['eip155:11155111'], // 👈 PENTING!
-                  events: ['accountsChanged', 'chainChanged'],
-                },
-              },
-            }}
-          />
         </SafeAreaProvider>
       </PaperProvider>
     </GestureHandlerRootView>
